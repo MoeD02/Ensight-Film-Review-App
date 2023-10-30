f1d0bc3ccc51c0da30917b16fc65e64f4403334b
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-10-29 22:13:47 -0700

    adjusting urlpatterns for static files

--- application/tempsite/tempsite/settings.py
+++ application/tempsite/tempsite/settings.py
@@ -137,15 +137,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = '/var/www/tempsite' # collectstatic saves here
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = '/var/www/tempsite/media'
 
 
--- application/tempsite/tempsite/urls.py
+++ application/tempsite/tempsite/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('about.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
