a52815945f4128e32f9ece18ade6b99352209a6c
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-10-29 22:30:43 -0700

    Revert "removing unneeded parts"
    This reverts commit d47876a1e93aed988619a86732339f160b7fd4ce.

--- application/tempsite/about/models.py
+++ application/tempsite/about/models.py
@@ -2,168 +2,62 @@ from django.db import models
 from django.conf import settings
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="profile",
-    )
-    
-    followers = models.ManyToManyField(
-        "self",
-        blank=True,
-        symmetrical=False,
-        related_name="followers",
-    )
-
-    following = models.ManyToManyField(
-        "self",
-        blank=True,
-        symmetrical=False,
-        related_name="following",
-    )
-
-    avatar = models.FileField(upload_to="avatars/")
-    bio = models.TextField(blank=True)
-
-
 class Genre(models.Model):
-    name = models.CharField(max_length=64)
-
-
+  name = models.CharField(max_length=64)
 class Movie(models.Model):
-    title = models.CharField(max_length=256)
-    poster_path = models.FileField(
-        upload_to="posters/",
-    )  # uploads to MEDIA_ROOT/posters/
-    release_date = models.DateField()
-    genres = models.ManyToManyField(
-        Genre,
-        related_name="genres",
-    )
-    description = models.CharField(max_length=1024)
-    rating_count = models.PositiveIntegerField(default=0)
-
-    rating_average = models.DecimalField(
-        max_digits=3,
-        decimal_places=2,
-        default=0,
-    )
-
-    def __str__(self):
-        return self.title
-
+  title = models.CharField(max_length=256)
+  release_date = models.DateField()
+  genres = models.ManyToManyField(Genre)
+  description = models.CharField(max_length=1024)
+  rating_count = models.PositiveSmallIntegerField(default=0)
+  rating_average = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+  poster_path = models.FileField(upload_to='posters/') # uploads to MEDIA_ROOT/posters/
+  
+  def __str__(self):
+    return self.title
 
 class Review(models.Model):
-    title = models.CharField(max_length=512)
-    text = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
-
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="reviews",
+  # related_name allows you to reverse lookup the relation, e.g. Movie.reviews.all() gets all reviews for that movie
+  # TODO: reverse lookup on user may need different syntax due to nature of django built-in user model
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+  movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+  title = models.CharField(max_length=512)
+  RATING_CHOICES = [
+    (5.0, "5"),
+    (4.5, "4.5"),
+    (4.0, "4"),
+    (3.5, "3.5"),
+    (3.0, "3"),
+    (2.5, "2.5"),
+    (2.0, "2"),
+    (1.5, "1.5"),
+    (1.0, "1"),
+    (0.5, "0.5"),
+    (0.0, "0"),
+  ]
+  rating = models.DecimalField(
+    max_digits=2,
+    decimal_places=1,
+    choices=RATING_CHOICES,
     )
-
-    movie = models.ForeignKey(
-        Movie,
-        on_delete=models.CASCADE,
-        related_name="reviews",
-    )
-
-    RATING_CHOICES = [
-        (5.0, "5"),
-        (4.5, "4.5"),
-        (4.0, "4"),
-        (3.5, "3.5"),
-        (3.0, "3"),
-        (2.5, "2.5"),
-        (2.0, "2"),
-        (1.5, "1.5"),
-        (1.0, "1"),
-        (0.5, "0.5"),
-        (0.0, "0"),
-    ]
-    rating = models.DecimalField(
-        max_digits=2,
-        decimal_places=1,
-        choices=RATING_CHOICES,
-    )
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["author", "movie"],
-                name="unique_review",
-            ),
-        ]
-
+  text = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return self.title
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="comments_by",
-    )
-
-    review = models.ForeignKey(
-        Review,
-        on_delete=models.CASCADE,
-        related_name="comments",
-    )
-
-    reply_to = models.ForeignKey(
-        "self",
-        on_delete=models.CASCADE,
-        null=True,
-        related_name="replies",
-    )
-
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
-
-
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+  review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+  text = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  
 class MovieList(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="lists",
-    )
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lists')
+  movies = models.ManyToManyField(Movie)
+  title = models.CharField(max_length=512)
+  description = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
 
-    movies = models.ManyToManyField(
-        Movie,
-        through="MovieListThrough",
-        related_name="lists",
-    )
-
-    title = models.CharField(max_length=512)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
-
-
-class MovieListThrough(models.Model):
-    movie = models.ForeignKey(
-        Movie,
-        on_delete=models.CASCADE,
-    )
-
-    movie_list = models.ForeignKey(
-        MovieList,
-        on_delete=models.CASCADE,
-    )
-
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["movie", "movie_list"],
-                name="unique_movie_in_list",
-            )
-        ]
