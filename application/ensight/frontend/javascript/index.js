6b4ac2081ca7cf6b1038eab7aa05314d3facfcc9
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-10-31 00:55:13 -0700

    implemented format for including react code

--- application/ensight/frontend/javascript/index.js
+++ application/ensight/frontend/javascript/index.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello from React</h1>,
+    document.getElementById('root')
+);
+
 function component() {
     const element = document.createElement('div');
     element.innerHTML = 'Hello webpack';
--- application/ensight/webpack.config.js
+++ application/ensight/webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
         filename: 'index-bundle.js',
         path: path.resolve(__dirname, './static/app/js'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+            },
+        ]
+    }
 };
+
