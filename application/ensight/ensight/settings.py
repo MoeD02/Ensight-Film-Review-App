3ae77d570a06cc37c3cf30a6ad5ed3003093d74c
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-10-31 00:18:58 -0700

    add templates and staticfile dirs

--- application/ensight/ensight/settings.py
+++ application/ensight/ensight/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'ensight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
