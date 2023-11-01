138499b3248a72a8d2ff2161ac02da79eb2132fc
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-01 00:23:57 -0700

    add settings for cors

--- application/ensight/ensight/settings.py
+++ application/ensight/ensight/settings.py
@@ -34,7 +34,11 @@ else:
         '.ensight.space',
     ]
 
+CORS_ALLOWED_ORIGINS = [
+    'localhost',
+]
 
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,9 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
