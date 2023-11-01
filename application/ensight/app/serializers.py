4082647086599624c7dc2078ab9960c4f75338df
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-01 01:50:15 -0700

    added serializers for user authentication

--- application/ensight/app/serializers.py
+++ application/ensight/app/serializers.py
@@ -1,5 +1,39 @@
 from .models import *
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model, authenticate
+
+UserModel = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel,
+        fields = '__all__'
+    def create(self, clean_data):
+        user_obj = UserModel.objects.create_user(
+            username=clean_data['username'],
+            email=clean_data['email'],
+            password=clean_data['password']
+            )
+        return user_obj
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def authenticate_user(self, clean_data):
+        user = authenticate(
+            username=clean_data['username'],
+            password=clean_data['password'],
+            )
+        if not user:
+            raise ValidationError('Could not authenticate user')
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email')
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
