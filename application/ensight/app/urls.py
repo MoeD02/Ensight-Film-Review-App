59c28c903659e82f0d9cff1876d5bde3c08e0018
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-05 18:17:30 -0800

    add urls for user auth api

--- application/ensight/app/urls.py
+++ application/ensight/app/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from knox import views as knox_views
 from . import views
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    path('api/register', views.UserCreate.as_view(), name='account-create'),
+    path('', include('knox.urls')),
+    path('account/register', views.RegisterAPI.as_view()),
+    path('account/current_user', views.CurrentUserAPI.as_view()),
+    path('account/login', views.LoginAPI.as_view()),
+    path('account/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
