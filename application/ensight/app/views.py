f86fe86b38302fce0e508a77049b4f872c08e74b
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-05 18:20:25 -0800

    add api views for user authentication

--- application/ensight/app/views.py
+++ application/ensight/app/views.py
@@ -1,50 +1,88 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.contrib.auth import get_user_model, authenticate, login, logout
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import permissions, status
+
 from .serializers import *
 from .forms import SearchForm
 from .models import *
+from django.contrib.auth import get_user_model
+
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+
+from knox.models import AuthToken
+
+from .serializers import *
 
 User = get_user_model()
 
-class UserCreate(APIView):
-    def post(self, request, format='json'):
-        serializer = UserSerializer(data=request.data)
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        
         if serializer.is_valid():
             user = serializer.save()
-            if user:
-                token = Token.objects.create(user=user)
-                json = serializer.data
-                json['token'] = token.key
-                return Response(json, status=status.HTTP_201_CREATED)
+            return Response({
+                'user':     serializer.data,
+                'token':    AuthToken.objects.create(user)[1],
+            })
+        return Response({'errors': serializer.errors}, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPI(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            return Response ({
+                'user':     UserSerializer(user).data,
+                'token':    AuthToken.objects.create(user)[1],
+            })
+        return Response({'errors': 'Invalid Credentials'}, status.HTTP_400_BAD_REQUEST)
+
+
+class CurrentUserAPI(RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+    
+    def get_object(self):
+        return self.request.user
 
 class HomeView(TemplateView):
-    template_name = 'app/home.html'
+    template_name = "app/home.html"
+
+
+def index(request):
+    return render(request, 'app/index.html')
 
 def home(request):
-    movie_list = Movie.objects.order_by('-release_date')[:10]
-    return render(request, 'app/home.html', {'movie_list': movie_list})
+    movie_list = Movie.objects.order_by("-release_date")[:10]
+    return render(request, "app/home.html", {"movie_list": movie_list})
+
 
 def search(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         form = SearchForm(request.GET)
         if form.is_valid():
-            query = form.cleaned_data['query']
-            search_type = form.cleaned_data['search_type']
-            
-            if search_type == 'movie':
+            query = form.cleaned_data["query"]
+            search_type = form.cleaned_data["search_type"]
+
+            if search_type == "movie":
                 results = Movie.objects.filter(title__icontains=query)
-            elif search_type == 'user':
+            elif search_type == "user":
                 results = Review.objects.filter(author__username__icontains=query)
-            
-            return render(request, 'app/home.html', {'results': results, 'search_type':search_type, 'form': form})
+
+            return render(
+                request,
+                "app/home.html",
+                {"results": results, "search_type": search_type, "form": form},
+            )
     else:
         form = SearchForm()
-    
-    return render(request, 'app/home.html', {'form': form})
+
+    return render(request, "app/home.html", {"form": form})
