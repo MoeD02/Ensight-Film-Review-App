07ea1ea28dfa816db06b868165de28ed831194c3
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-05 20:00:39 -0800

    fixed accounts urls

--- application/ensight/app/urls.py
+++ application/ensight/app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('', include('knox.urls')),
-    path('account/register', views.RegisterAPI.as_view()),
-    path('account/current_user', views.CurrentUserAPI.as_view()),
-    path('account/login', views.LoginAPI.as_view()),
-    path('account/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('accounts/register', views.RegisterAPI.as_view()),
+    path('accounts/current_user', views.CurrentUserAPI.as_view()),
+    path('accounts/login', views.LoginAPI.as_view()),
+    path('accounts/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
