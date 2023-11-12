46f01c63d1e6005010c37e038d420c9a59e6d0c6
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-11 21:21:19 -0800

    added authToken retrieval

--- application/ensight/frontend/src/App.js
+++ application/ensight/frontend/src/App.js
@@ -6,28 +6,28 @@
 //import Home from './pages/Home';
 //import DisplayMovie from './pages/DisplayMovie.js'
 //import MovieLanding from './pages/MovieLanding.js'
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Header from './components/Header';
-import FooterComponent from './components/FooterComponent';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import FooterComponent from "./components/FooterComponent";
 
-import Home from './pages/Home';
-import Profile from './pages/Profile'; 
-import Browse from './pages/Browse';
-import DisplayList from './pages/DisplayList';
-import DisplayMovie from './pages/DisplayMovie';
-import DisplayUser from './pages/DisplayUser';
-import LoginPage from './pages/LoginPage';
-import './App.css';
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Browse from "./pages/Browse";
+import DisplayList from "./pages/DisplayList";
+import DisplayMovie from "./pages/DisplayMovie";
+import DisplayUser from "./pages/DisplayUser";
+import LoginPage from "./pages/LoginPage";
+import "./App.css";
 
 function App() {
-    const [authToken, setAuthToken] = useState('');
-    useEffect(() => {
-        const token = localStorage.getItem('Authorization');
-        if(token) {
-            setAuthToken(token);
-        }
-    }, [])
+  const [authToken, setAuthToken] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("Authorization");
+    if (token) {
+      setAuthToken(token);
+    }
+  }, []);
   return (
     <Router>
       <Header />
