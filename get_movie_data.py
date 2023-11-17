77f4c16e5ed675058226a7b4731a56b66554bb92
Alex del Rio <adelrio3@mail.sfsu.edu>
2023-11-17 12:07:49 -0800

    fixed: script now working correctly

--- get_movie_data.py
+++ get_movie_data.py
@@ -1,49 +1,116 @@
+import asyncio
+import aiohttp
+import aiofiles
+from aiofiles.threadpool.text import AsyncTextIOWrapper as AIOFile
+
 import json
-import requests
+from typing import List
 
-# TODO: make filepaths relative to project directory (except keyfile)
 
-# Base url for API request
-base_url = "https://api.themoviedb.org/3/movie/"
+FILEPATH = './ensight/app/fixtures/movie_fixture.json'
 
-# Get API credentials from local file
-with open("./tmdb_API_key.txt", "r") as keyfile:
-    token = keyfile.read().strip()
+with open('./tmdb_API_key.txt', 'r') as f:
+    token = f.read().strip()
+
+with open('./urls.txt', 'r') as f:
+    urls = f.read().splitlines()
+
+URLS_LEN = len(urls) - 1
 
 headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer " + token,
+    'Authorization': 'Bearer ' + token,
 }
 
-"""
-Open dump of movie ids downloaded from:
-    http://files.tmdb.org/p/exports/movie_ids_{month}_{day}_{year}.json.gz
-Each line is expected to be a valid JSON object (NOT the entire file).
-Request data from TMDB for each movie ID and fill properly formatted dictionary
-to generate fixture for Django to instantiate model objects.
-"""
-formatted_data = []  # Collect formatted dictionaries
-with open("./movie_ids_11_13_2023.json", 'r') as infile:
-    for line in infile:
-        query = json.loads(line)
-        if not query['video']:  # video == false -> is a movie
-            url = base_url + str(query['id'])
-            response = requests.get(url, headers=headers)
-            data = response.json()
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
-            temp = {}
-            temp['model'] = "app.movie"
-            temp['pk'] = data['id']
-            temp['fields'] = {
-                'title': data['title'],
-                'poster_path': data['poster_path'],
-                'backdrop_path': data['backdrop_path'] if data['backdrop_path'] else data['poster_path'],
-                'genres': [e['id'] for e in data['genres']],
-                'release_date': data['release_date'],
-                'description': data['overview'],
-            }
-        formatted_data.append(temp)
 
-# Write to fixtures directory as JSON
-with open('./application/ensight/app/fixtures/movie_fixture.json', 'w') as outfile:
-    json.dump(formatted_data, outfile, indent=2)  # indent for human-readability
+async def init_script():
+    async with aiofiles.open(FILEPATH, mode='w+', encoding='utf-8') as outfile:
+        await outfile.write('[')
+        await execute_fetcher_tasks(outfile)
+        await outfile.seek(-2,2)
+        await outfile.truncate()
+        await outfile.write('\n]')
+        await outfile.close()
+
+
+async def execute_fetcher_tasks(outfile: AIOFile):
+    session_timeout = aiohttp.ClientTimeout(total=None)
+    async with aiohttp.ClientSession(headers=headers, timeout=session_timeout) as session:
+        task_list = await create_tasks(session, urls, outfile)
+        await asyncio.gather(*task_list)
+
+
+async def create_tasks(session: aiohttp.ClientSession, urls:List[str], outfile: AIOFile) -> List[asyncio.Task]:
+    task_list = []
+    for i, url in enumerate(urls):
+        task = asyncio.create_task(fetch_and_save_data(session, url, outfile, i))
+        task_list.append(task)
+        printProgressBar(i, URLS_LEN, prefix='Tasks ')
+    return task_list
+
+async def fetch_and_save_data(session: aiohttp.ClientSession, url: str, outfile:AIOFile, i: int):
+    printProgressBar(i, URLS_LEN, prefix='Data  ')
+    data = None
+    while data is None:
+        try:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                data = await response.json()
+        except aiohttp.ClientError:
+            e = response.status
+            print(f'Error {e}: {url}')
+            if e == 404:
+                return
+            await asyncio.sleep(1)
+    
+    temp = {}
+    temp['model'] = "app.movie"
+    temp['pk'] = data['id']
+    temp['fields'] = {
+        'title': data['title'],
+        'poster_path': data['poster_path'],
+        'backdrop_path': data['backdrop_path'],
+        'genres': [e['id'] for e in data['genres']],
+        'release_date': data['release_date'] if not data['release_date'] == "" else None,
+        'description': data['overview'],
+        'runtime': data['runtime'],
+        'rating_count': data['vote_count'],
+        'rating_average': data['vote_average'],
+        'popularity': data['popularity'],
+        'trailer_path': None,
+        'director': None,
+    }
+    for e in data['videos']['results']:
+        if e['type'] == 'Trailer' and e['official'] and e['site'] == 'YouTube':
+            temp['fields']['trailer_path'] = e['key']
+            break
+    for e in data['credits']['crew']:
+        if e['job'] == 'Director':
+            temp['fields']['director'] = e['id']
+            break
+    await outfile.write(json.dumps(temp) + ',\n')
+    await outfile.flush()
+
+
+if __name__ == '__main__':
+    asyncio.run(init_script())
