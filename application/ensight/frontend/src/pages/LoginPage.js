eb745debd51ac97c3f09cbe7c054f56e1a43e6f7
ajdelrio <alex.j.delrio@gmail.com>
2023-12-08 20:11:56 -0800

    should add api in front of paths

--- application/ensight/frontend/src/pages/LoginPage.js
+++ application/ensight/frontend/src/pages/LoginPage.js
@@ -18,7 +18,7 @@ const LoginPage = () => {
 
 	const handleLogin = async (e) => {
 		e.preventDefault();
-		const loginEndpoint = "http://127.0.0.1:8000/accounts/login";
+		const loginEndpoint = "http://127.0.0.1:8000/api/accounts/login";
 		const userData = {
 			username,
 			password,
@@ -48,7 +48,7 @@ const LoginPage = () => {
 
 	const handleRegister = async (e) => {
 		e.preventDefault();
-		const RegisterEndpoint = "http://127.0.0.1:8000/accounts/register";
+		const RegisterEndpoint = "http://127.0.0.1:8000/api/accounts/register";
 		const userData = {
 			username,
 			email,
