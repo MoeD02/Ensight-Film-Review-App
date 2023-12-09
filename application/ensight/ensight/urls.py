16f0d219546331d9187019e6f05b8ef223a0ff3f
Ubuntu <ubuntu@ip-172-31-7-9.us-west-1.compute.internal>
2023-12-09 04:13:31 +0000

    include api on app urls

--- application/ensight/ensight/urls.py
+++ application/ensight/ensight/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('api/', include('app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
